6c5149f15405306c4ab0fc114bcfd68961a4ded1
Arseniy Plaksin <arsp93@gmail.com>
2014-09-26 09:57:16 +0000

    fixed signup bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,5 +80,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => 'APP_NAME.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'tutorialapp1.herokuapp.com/' }
 end
